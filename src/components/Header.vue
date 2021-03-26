2e645da0c369ec4bc599d779a8cb3517bfabd28b
iqbalabdurrazaq <mancunianmerchant816@gmail.com>
2021-03-26 21:36:25 +0700

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,45 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+    <Button
+      v-show="homePage"
+      @btn-click="$emit('toggle-add-task')"
+      :text="showAddTask ? 'Close Task' : 'Add Task'"
+      :color="showAddTask ? 'red' : 'green'"
+    />
+  </header>
+</template>
+
+<script>
+import Button from "./Button.vue";
+
+export default {
+  components: { Button },
+  name: "Header",
+  props: {
+    title: {
+      type: String,
+      default: "Hello",
+    },
+    showAddTask: Boolean,
+  },
+  computed: {
+    homePage() {
+      if (this.$route.path === "/") {
+        return true;
+      } else {
+        return false;
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
---
+++ src/components/Tasks.vue
@@ -0,0 +1,21 @@
+<template>
+  <div :key="task_data.id" v-for="task_data in tasks">
+    <Task
+      @toggle-reminder="$emit('toggle-reminder', task_data.id)"
+      @delete-task="$emit('delete-task', task_data.id)"
+      :task_items="task_data"
+    />
+  </div>
+</template>
+
+<script>
+import Task from "./Task.vue";
+export default {
+  components: { Task },
+  name: "Tasks",
+  props: {
+    tasks: Array,
+  },
+  emits: ["delete-task", "toggle-reminder"],
+};
+</script>
